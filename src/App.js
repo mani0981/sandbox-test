0112c5066b379f80917e4c40e5e219a235376e28
Manish Chawla <manishchawla09@gmail.com>
2020-12-03 20:08:11 -0800

    update the state adding the toggle button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { displayBio: true };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen! Chaange</h2>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in San Jose, I am a hard worker.</p>
+            <p>My favorit language HTML/CSS.</p>
+            <p>Besides coding, i am alos trying to learn Guitar </p>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
+
+export default App;
